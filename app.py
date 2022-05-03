69cf1fbb09140266c17a049a8523f02d8c0d05cb
jarsham-ml <jarsham5@gmail.com>
2022-05-04 00:27:43 +0530

    update app

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import validators
+from wtforms import FileField, SubmitField
 
 app = Flask(__name__)
 
 app.config(
     SECRET_KEY = "j234jkl23j"
 )
+
+class fileForm(FlaskForm):
+    xlsxfile = ileField(u'Image File', [validators.regexp(u'^[^/\\]\.jpg$')])
 
 @app.route('/')
 def home():
